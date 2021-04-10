138aa106c0c1bbb8d6850b37b6a952d376e9c811
annachk <annachk@terpmail.umd.edu>
2021-04-10 00:49:02 -0300

    chart

--- public/scripts.js
+++ public/scripts.js
@@ -1,138 +1,3 @@
-
-// const chart = new scripts.Chart("chartContainer", {
-//     animationEnabled: true,
-//     title:{
-//         text: "Random meals and their macros"
-//     },
-//     axisX: {
-//         valueFormatString: ""
-//     },
-//     axisY: {
-//         prefix: ""
-//     },
-//     toolTip: {
-//         shared: true
-//     },
-//     legend:{
-//         cursor: "pointer",
-//         itemclick: toggleDataSeries
-//     },
-//     data: [{
-//         type: "stackedBar",
-//         name: "Calories",
-//         showInLegend: "true",
-//         // xValueFormatString: "DD, MMM",
-//         // yValueFormatString: "$#,##0",
-//         dataPoints: [
-//             { x: new meals[0][calories], y: meals[0][meal_name] },
-            // { x: new Date(2017, 0, 31), y: 45 },
-            // { x: new Date(2017, 1, 1), y: 71 },
-            // { x: new Date(2017, 1, 2), y: 41 },
-            // { x: new Date(2017, 1, 3), y: 60 },
-            // { x: new Date(2017, 1, 4), y: 75 },
-            // { x: new Date(2017, 1, 5), y: 98 }
-        // ]
-    // },
-    // {
-    //     type: "stackedBar",
-    //     name: "Snacks",
-    //     showInLegend: "true",
-    //     xValueFormatString: "DD, MMM",
-    //     yValueFormatString: "$#,##0",
-    //     dataPoints: [
-    //         { x: new Date(2017, 0, 30), y: 86 },
-    //         { x: new Date(2017, 0, 31), y: 95 },
-    //         { x: new Date(2017, 1, 1), y: 71 },
-    //         { x: new Date(2017, 1, 2), y: 58 },
-    //         { x: new Date(2017, 1, 3), y: 60 },
-    //         { x: new Date(2017, 1, 4), y: 65 },
-    //         { x: new Date(2017, 1, 5), y: 89 }
-    //     ]
-    // },
-    // {
-    //     type: "stackedBar",
-    //     name: "Drinks",
-    //     showInLegend: "true",
-    //     xValueFormatString: "DD, MMM",
-    //     yValueFormatString: "$#,##0",
-    //     dataPoints: [
-    //         { x: new Date(2017, 0, 30), y: 48 },
-    //         { x: new Date(2017, 0, 31), y: 45 },
-    //         { x: new Date(2017, 1, 1), y: 41 },
-    //         { x: new Date(2017, 1, 2), y: 55 },
-    //         { x: new Date(2017, 1, 3), y: 80 },
-    //         { x: new Date(2017, 1, 4), y: 85 },
-    //         { x: new Date(2017, 1, 5), y: 83 }
-    //     ]
-    // },
-    // {
-    //     type: "stackedBar",
-    //     name: "Dessert",
-    //     showInLegend: "true",
-    //     xValueFormatString: "DD, MMM",
-    //     yValueFormatString: "$#,##0",
-    //     dataPoints: [
-    //         { x: new Date(2017, 0, 30), y: 61 },
-    //         { x: new Date(2017, 0, 31), y: 55 },
-    //         { x: new Date(2017, 1, 1), y: 61 },
-    //         { x: new Date(2017, 1, 2), y: 75 },
-    //         { x: new Date(2017, 1, 3), y: 80 },
-    //         { x: new Date(2017, 1, 4), y: 85 },
-    //         { x: new Date(2017, 1, 5), y: 105 }
-    //     ]
-    // },
-    // {
-    //     type: "stackedBar",
-    //     name: "Takeaway",
-    //     showInLegend: "true",
-    //     xValueFormatString: "DD, MMM",
-    //     yValueFormatString: "$#,##0",
-    //     dataPoints: [
-    //         { x: new Date(2017, 0, 30), y: 52 },
-    //         { x: new Date(2017, 0, 31), y: 55 },
-    //         { x: new Date(2017, 1, 1), y: 20 },
-    //         { x: new Date(2017, 1, 2), y: 35 },
-    //         { x: new Date(2017, 1, 3), y: 30 },
-    //         { x: new Date(2017, 1, 4), y: 45 },
-    //         { x: new Date(2017, 1, 5), y: 25 }
-    //     ]
-//     }]
-// });
-// chart.render();
-
-// function toggleDataSeries(e) {
-//     if(typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
-//         e.dataSeries.visible = false;
-//     }
-//     else {
-//         e.dataSeries.visible = true;
-//     }
-//     chart.render();
-// }
-
-
-// //////////////////////////////////////////////////////////////////////////////////
-
-async function populateRestaurants() {
-    const diningRequest = await fetch('api/meals');
-    const diningData = await diningRequest.json();
-
-    diningData.data.forEach((restaurant) => {
-        const appendItem = document.createElement('div');
-        appendItem.classList.add('title', 'has-text-centered', 'is-parent', 'is-3');
-        appendItem.innerHTML = `
-            <article class="title is-child box has-background-link-dark">
-            <span class="subtitle has-text-light has-text-weight-bold">
-                ${restaurant.hall_name.split(',')[0]}
-            <br />
-            <span class="has-text-light">
-                ${restaurant.hall_address.split(',')[1]}
-            </span>
-            </article>`
-        targetBox.append(appendItem);
-    });
-}
-
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -151,28 +16,34 @@ async function windowActions() {
     const results = await getMeals();
     const meals = results.data;
 
+    //Chart
     const mealArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    let calories_array = []
+    let serving_array = []
+    let cholesterol_array = []
+
     const selectedMeals = mealArray.map((element) => {
         const random = getRandomIntInclusive(0, meals.length - 1);
         meals_random = meals[random]
+
+        calories_array.push({calories_k: meals_random["calories"], name_k: meals_random["meal_name"]})
+        serving_array.push({serving_k: meals_random["serving_size"], name_k: meals_random["meal_name"]})
+        cholesterol_array.push({cholesterol_k: meals_random["cholesterol"], name_k: meals_random["meal_name"]})
+
         return meals_random;
     });
-
     console.table(selectedMeals);
-
-    //////FROM TABLE TO GRAPH/////////////////////////////////////////////////////////////////////////////////////////////
-
-    var chart = new CanvasJS.Chart("chartContainer", {
+    
+    console.log(calories_array)
+    console.log(serving_array)
+    console.log(cholesterol_array)
+    
+    var chart = new CanvasJS.Chart("chartContainer",{
         animationEnabled: true,
         title:{
-            text: "Evening Sales in a Restaurant"
+            text: "Meals x Macros"
         },
-        // axisX: {
-        //     valueFormatString: "DDD"
-        // },
-        // axisY: {
-        //     prefix: "$"
-        // },
         toolTip: {
             shared: true
         },
@@ -182,85 +53,55 @@ async function windowActions() {
         },
         data: [{
             type: "stackedBar",
-            name: "Meals",
+            name: "calories",
             showInLegend: "true",
-            // xValueFormatString: "DD, MMM",
-            // yValueFormatString: "$#,##0",
-            dataPoints: [
-                { x: new results.data.meals_random.calories, y: meals_random[0]["meal_name"] },
-                { x: new Date(2017, 0, 31), y: 45 },
-                { x: new Date(2017, 1, 1), y: 71 },
-                { x: new Date(2017, 1, 2), y: 41 },
-                { x: new Date(2017, 1, 3), y: 60 },
-                { x: new Date(2017, 1, 4), y: 75 },
-                { x: new Date(2017, 1, 5), y: 98 }
+            dataPoints: [{y: calories_array[0]["calories_k"], label: calories_array[0]["name_k"]},
+                {y: calories_array[1]["calories_k"], label: calories_array[1]["name_k"]},
+                {y: calories_array[2]["calories_k"], label: calories_array[2]["name_k"]},
+                {y: calories_array[3]["calories_k"], label: calories_array[3]["name_k"]},
+                {y: calories_array[4]["calories_k"], label: calories_array[4]["name_k"]},
+                {y: calories_array[5]["calories_k"], label: calories_array[5]["name_k"]},
+                {y: calories_array[6]["calories_k"], label: calories_array[6]["name_k"]},
+                {y: calories_array[7]["calories_k"], label: calories_array[7]["name_k"]},
+                {y: calories_array[8]["calories_k"], label: calories_array[8]["name_k"]},
+                {y: calories_array[9]["calories_k"], label: calories_array[9]["name_k"]}
             ]
         },
         {
             type: "stackedBar",
-            name: "Snacks",
+            name: "serving_size",
             showInLegend: "true",
-            xValueFormatString: "DD, MMM",
-            yValueFormatString: "$#,##0",
-            dataPoints: [
-                { x: new Date(2017, 0, 30), y: 86 },
-                { x: new Date(2017, 0, 31), y: 95 },
-                { x: new Date(2017, 1, 1), y: 71 },
-                { x: new Date(2017, 1, 2), y: 58 },
-                { x: new Date(2017, 1, 3), y: 60 },
-                { x: new Date(2017, 1, 4), y: 65 },
-                { x: new Date(2017, 1, 5), y: 89 }
+            dataPoints: [{y: serving_array[0]["serving_k"], label: serving_array[0]["name_k"]},
+            {y: serving_array[1]["serving_k"], label: serving_array[1]["name_k"]},
+            {y: serving_array[2]["serving_k"], label: serving_array[2]["name_k"]},
+            {y: serving_array[3]["serving_k"], label: serving_array[3]["name_k"]},
+            {y: serving_array[4]["serving_k"], label: serving_array[4]["name_k"]},
+            {y: serving_array[5]["serving_k"], label: serving_array[5]["name_k"]},
+            {y: serving_array[6]["serving_k"], label: serving_array[6]["name_k"]},
+            {y: serving_array[7]["serving_k"], label: serving_array[7]["name_k"]},
+            {y: serving_array[8]["serving_k"], label: serving_array[8]["name_k"]},
+            {y: serving_array[9]["serving_k"], label: serving_array[9]["name_k"]}
             ]
         },
         {
             type: "stackedBar",
-            name: "Drinks",
+            name: "cholesterol",
             showInLegend: "true",
-            xValueFormatString: "DD, MMM",
-            yValueFormatString: "$#,##0",
-            dataPoints: [
-                { x: new Date(2017, 0, 30), y: 48 },
-                { x: new Date(2017, 0, 31), y: 45 },
-                { x: new Date(2017, 1, 1), y: 41 },
-                { x: new Date(2017, 1, 2), y: 55 },
-                { x: new Date(2017, 1, 3), y: 80 },
-                { x: new Date(2017, 1, 4), y: 85 },
-                { x: new Date(2017, 1, 5), y: 83 }
+            dataPoints: [{y: cholesterol_array[0]["cholesterol_k"], label: cholesterol_array[0]["name_k"]},
+            {y: cholesterol_array[1]["cholesterol_k"], label: cholesterol_array[1]["name_k"]},
+            {y: cholesterol_array[2]["cholesterol_k"], label: cholesterol_array[2]["name_k"]},
+            {y: cholesterol_array[3]["cholesterol_k"], label: cholesterol_array[3]["name_k"]},
+            {y: cholesterol_array[4]["cholesterol_k"], label: cholesterol_array[4]["name_k"]},
+            {y: cholesterol_array[5]["cholesterol_k"], label: cholesterol_array[5]["name_k"]},
+            {y: cholesterol_array[6]["cholesterol_k"], label: cholesterol_array[6]["name_k"]},
+            {y: cholesterol_array[7]["cholesterol_k"], label: cholesterol_array[7]["name_k"]},
+            {y: cholesterol_array[8]["cholesterol_k"], label: cholesterol_array[8]["name_k"]},
+            {y: cholesterol_array[9]["cholesterol_k"], label: cholesterol_array[9]["name_k"]}
             ]
-        },
-        {
-            type: "stackedBar",
-            name: "Dessert",
-            showInLegend: "true",
-            xValueFormatString: "DD, MMM",
-            yValueFormatString: "$#,##0",
-            dataPoints: [
-                { x: new Date(2017, 0, 30), y: 61 },
-                { x: new Date(2017, 0, 31), y: 55 },
-                { x: new Date(2017, 1, 1), y: 61 },
-                { x: new Date(2017, 1, 2), y: 75 },
-                { x: new Date(2017, 1, 3), y: 80 },
-                { x: new Date(2017, 1, 4), y: 85 },
-                { x: new Date(2017, 1, 5), y: 105 }
-            ]
-        },
-        {
-            type: "stackedBar",
-            name: "Takeaway",
-            showInLegend: "true",
-            xValueFormatString: "DD, MMM",
-            yValueFormatString: "$#,##0",
-            dataPoints: [
-                { x: new Date(2017, 0, 30), y: 52 },
-                { x: new Date(2017, 0, 31), y: 55 },
-                { x: new Date(2017, 1, 1), y: 20 },
-                { x: new Date(2017, 1, 2), y: 35 },
-                { x: new Date(2017, 1, 3), y: 30 },
-                { x: new Date(2017, 1, 4), y: 45 },
-                { x: new Date(2017, 1, 5), y: 25 }
-            ]
-        }]
+        }
+    ]
     });
+    
     chart.render();
     
     function toggleDataSeries(e) {
@@ -272,7 +113,6 @@ async function windowActions() {
         }
         chart.render();
     }
-////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
 
 window.onload = windowActions;
